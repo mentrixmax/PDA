fadb5b46d587acf3ad5fbddf668c6dbbb16bcca0
mentrixmax <mateus@les13>
2015-12-18 12:39:37 -0200

    teset

---
+++ CMakeFiles/PDA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/PDA.dir/PDA_PDI.cpp.o"
+  "PDA.pdb"
+  "PDA"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/PDA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
